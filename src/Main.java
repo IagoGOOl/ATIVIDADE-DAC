739ced539c7b943b06e44441978fde5e71fbc68d
Warlla <90108466+WarllaK@users.noreply.github.com>
2024-03-23 13:55:56 -0300

    atualizando crud ator

--- src/Main.java
+++ src/Main.java
@@ -13,14 +13,51 @@ public class Main {
 		
 		//CRUD Filme:
 		
-		//Criando funcionario
+		//Criando Filme
 		Filme filme = new Filme();
-		filme.setTitulo("Velozes e furiosos 5");
+		filme.setTitulo("socorro deus");
 		filme.setGenero("ação");
 		filme.setAnoLancamento("2011");
 		
 		FilmeDAO filmeDao = new FilmeDAO();
 		filmeDao.criar(filme);
+		
+		
+		
+		// Lendo instancias de atores: 
+		List<Filme> filmes = filmeDao.findAll();
+		
+		for (Filme f: filmes) {
+			System.out.println();
+			System.out.println("Id: " + f.getId());
+			System.out.println("Titulo: " + f.getTitulo());
+			System.out.println("Genero: " + f.getGenero());
+			System.out.println("Ano de lançamento:: " + f.getAnoLancamento());
+			System.out.println();
+			System.out.println(f.toString());
+		}
+		
+		
+		// Atualizando instancias:
+		
+		Filme filmeExiste = filmeDao.findById(5L);
+		
+		Filme updateFilme = new Filme();
+		updateFilme.setId(5L);
+		updateFilme.setTitulo("Testando UPDATE");
+		updateFilme.setGenero("ação");
+		updateFilme.setAnoLancamento("2011");
+		
+		filmeDao.atualizar(updateFilme);
+		
+		// Deletando instancias:
+		Filme filmeJaExiste= filmeDao.findById(1L);
+		filmeJaExiste.setId(1L);
+		filmeDao.apagar(filmeJaExiste);
+		
+		
+		
+		
 
 
 		// CRUD ATOR:
@@ -34,6 +71,8 @@ public class Main {
 
 		AtorDAO atorDAO = new AtorDAO();
 		atorDAO.criar(ator);
+		
+
 		
 	}
 
